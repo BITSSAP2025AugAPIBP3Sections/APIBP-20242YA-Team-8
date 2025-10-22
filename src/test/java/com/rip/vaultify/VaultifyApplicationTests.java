d717b9637ab4a0e17b35959c9a1caad382e14f49
harshithcodes <kuvvamh@gmail.com>
2025-10-22 21:09:31 +0530

    project structure

---
+++ src/test/java/com/rip/vaultify/VaultifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rip.vaultify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
