562810cae3c986105b5ce4f0821c550d4f4d1b69
harshithcodes <kuvvamh@gmail.com>
2025-11-06 22:40:23 +0530

    added swagger config

---
+++ backend/src/main/java/com/rip/vaultify/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.rip.vaultify.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Vaultify API",
+                version = "v1",
+                description = "REST API for file storage, sharing permissions, and version control.",
+                contact = @Contact(name = "Vaultify"),
+                license = @License(name = "MIT")
+        ),
+        security = {@SecurityRequirement(name = "bearerAuth")}
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
+
+
